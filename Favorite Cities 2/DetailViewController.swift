9a71bee6286d3ac3b3e9b35042842e9ad2ce3051
Sydney Ripsky <sripsky@bsd220.org>
2020-01-22 10:42:30 -0600

    Initial Commit

---
+++ Favorite Cities 2/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Favorite Cities 2
+//
+//  Created by Sydney Ripsky on 1/22/20.
+//  Copyright © 2020 Sydney Ripsky. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
